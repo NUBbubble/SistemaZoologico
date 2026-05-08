623b8d0ac684102ffad1fdb9456ee9cebdb96972
NUBbubble <brayanrojasmunoz78@gmail.com>
2026-05-08 15:49:02 -0600

    Barra Lateral View; Ahora automatiza la accion de enviarle un animal, crea su respectivo espacio

--- Vista/BarraLateralView.java
+++ Vista/BarraLateralView.java
@@ -2,15 +2,25 @@ package Vista;
 
 import java.awt.*;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JSplitPane;
+import javax.swing.SwingConstants;
+import javax.swing.border.Border;
 
 import Modelo.Animal;
+import Modelo.Herviboro;
 
 public class BarraLateralView extends JPanel{
+    private final int WIDTH=300;
+    private final int ALTURA_DE_SELECCION=30,ALTURA_DE_BOTON=20; 
     public BarraLateralView(){
-        setPreferredSize(new Dimension(300,800));
-        crearSelecciones(null);
+        setPreferredSize(new Dimension(WIDTH,800));
+        Herviboro perolio  =new Herviboro("Perolio","Herviboro",32,210,110);
+        Animal[] elanimal={perolio};
+        crearSelecciones(elanimal);
     }
     @Override
     protected void paintComponent(Graphics g) {
@@ -23,9 +33,28 @@ public class BarraLateralView extends JPanel{
         g.fillRect(0,0,getWidth(),getHeight());
     }
     public void crearSelecciones(Animal[] misAnimales){
-        JPanel 
-        JButton Boton=new JButton("Leon");
-        Boton.setPreferredSize(getPreferredSize());
-        add(Boton);
+        JPanel panel= new JPanel(new GridLayout(misAnimales.length,1));
+        panel.setPreferredSize(new Dimension(WIDTH,(misAnimales.length*50)));
+        
+        for (Animal animal:misAnimales){
+            JPanel seleccion=new JPanel(new GridLayout(1, 2));
+            seleccion.setPreferredSize(new Dimension(WIDTH, ALTURA_DE_SELECCION));
+                seleccion.setBackground(new Color(220,230,228));
+                seleccion.setBorder(BorderFactory.createEmptyBorder(10,5,10,5));
+            //Creacion de botones
+            JButton boton=new JButton(animal.getNombre());
+                boton.setPreferredSize(new Dimension(1,ALTURA_DE_BOTON));
+                boton.setBackground(new Color(220,230,228));
+            //Creacion Del Texto Especie
+            JLabel especie= new JLabel(animal.getEspecie());
+                especie.setPreferredSize(new Dimension((WIDTH/3)*2,ALTURA_DE_BOTON));
+                especie.setBackground(new Color(220,230,228));
+                especie.setHorizontalAlignment(SwingConstants.RIGHT);
+            seleccion.add(boton);
+            seleccion.add(especie);
+            panel.add(seleccion);
+        }
     }
+
 }
+
